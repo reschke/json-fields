6ebdac103695017620e7f52fac96a26951c722c0
Julian Reschke <julian.reschke@gmx.de>
2021-03-21 16:01:25 +0100

    use String.join

--- src/main/java/org/greenbytes/http/jfv/Parser.java
+++ src/main/java/org/greenbytes/http/jfv/Parser.java
@@ -8,23 +8,13 @@ import javax.json.JsonReader;
 
 public class Parser {
 
-    // naive impl for now; use streaming API to avoid the string operations
-
     public static JsonArray parse(String input) {
         JsonReader result = Json.createReader(new StringReader("[" + input + "]"));
         return result.readArray();
     }
 
     public static JsonArray parse(String... input) {
-        StringBuilder b = new StringBuilder("[");
-        String delim = "";
-        for (String s : input) {
-            b.append(delim);
-            delim = ",";
-            b.append(s);
-        }
-        b.append("]");
-        JsonReader result = Json.createReader(new StringReader(b.toString()));
+        JsonReader result = Json.createReader(new StringReader("[" + String.join(",", input) + "]"));
         return result.readArray();
     }
 }
